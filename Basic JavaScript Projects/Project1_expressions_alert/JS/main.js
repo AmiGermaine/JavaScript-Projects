16aac40fdc27e065e360c11bbc0de7d29abb7f1a
Amida <143059696+AmiGermaine@users.noreply.github.com>
2023-10-02 17:11:54 -0700

    assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,14 @@
+function My_First_Function() { //Defining a function and naming it
+    var str = "This is the button text!"; //Defining a variable and giving it 
+                                         //a string value
+    document.getElementById("Button_Text") .innerHTML = str; //Putting the value 
+                                            //of the variable into the HTML elementFromPoint
+                                            //with the "Button_Text" id
+}
+window.alert("Hello, there!") ;
+document.write("Bonjour!") ;
+var Sent1 = "This is the begining of the string" ;
+var Sent2 = "and this is the end of the string" ;
+document.write(Sent1 + Sent2);
+var X = 10 - 5 ; //This is an expression of X = 10 - 5
+document.write(X);
---
+++ Basic JavaScript Projects/Project1_expressions_alert/JavaScript_File.js
@@ -0,0 +1,24 @@
+alert("Hello World!") ;
+window.alert("Hello, there!") ;
+document.write("Bonjour!") ;
+var A = "this is a string";
+document.write(A) ;
+
+document.write("\"Be who you areand say what you feel," 
++ " because those who mind don\'t mater and those who matter don\'t mind.\"" 
++ "-Dr. Seuss") ;
+
+var B = "Concatenating" + "String"
+document.write(B)
+
+function My_First_Function() {
+    var str = "This is the button text!";
+    document.getElementById("Button_Text") .innerHTML = str;
+}
+
+window.alert('Bonjour!') ;
+
+var Family = "The Arezzing", Dad = "Jeremiah", Mom = "Hermoine", Daughter = "Penny", Son = "Zorro";
+document.write(Dad) ; 
+
+document.write(2 + 2); 
